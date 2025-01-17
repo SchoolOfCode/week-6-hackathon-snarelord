763926c61fe1b38678fb993f4401d311d46de886
Kit Jones <174150846+snarelord@users.noreply.github.com>
2025-01-17 10:43:48 +0000

    updated instructions

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 /*
+
+(make sure vitest is installed)
+
 Hi there! You've stumbled across a farm where all the ducks have escaped and the farmer needs your help to find them. 
 
 Your mission is to complete the findMissingDucks function that takes in an array of present duck IDs. This array is unsorted and possibly contains duplicates. 
@@ -10,6 +13,7 @@ Example
 Input (ducks that are NOT missing): [30, 23, 12, 10, 5, 28, 17, 2, 9, 11, 16, 39, 24, 21, 18]
 
 Complete the function below to see if it passes! 
+
 */
 
 
